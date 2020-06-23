557e169af2ff8c2bb82d2973bf403ee027bfa2ab
began91 <began91@yahoo.com>
2020-06-23 14:24:27 -0500

    Bug fix for column 2

--- src/components/ResultRow.js
+++ src/components/ResultRow.js
@@ -4,7 +4,7 @@ function ResultRow(props) {
     return (
         <tr className={(props.ext ? "ext" : "")}>
             <th className="row-head">{props.head}</th>
-            <td className={props.gray2 ? "gray col2" : "col2"}>{props.col2 && ""}</td>
+            <td className={props.gray2 ? "gray col2" : "col2"}>{props.col2}</td>
             <td>{props.col3}</td>
             <td>{props.col4}</td>
         </tr>
